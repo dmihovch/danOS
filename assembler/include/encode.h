139f29405fe4a7ac3615ad81942c6c323505a1a7
dmihovch <dmihovch@gmail.com>
2025-10-20 13:52:40 -0400

    really rethinking

--- assembler/include/encode.h
+++ assembler/include/encode.h
@@ -1,5 +1,4 @@
 #include <stdlib.h>
 #include "../include/token.h"
-#include "../include/labels.h"
 #include <string.h>
-int first_pass(tokens_t*, labels_t*);
+int first_pass(tokens_t*);
--- assembler/src/encode.c
+++ assembler/src/encode.c
@@ -1,25 +1,16 @@
 #include "../include/encode.h"
-#include <string.h>
 
 //probably change name to be more specific
-int first_pass(tokens_t * t, labels_t * l){
+int first_pass(tokens_t * t){
     int i;
     int sz = t->size;
     for(i = 0; i<sz; i++){
 
-        //need to check if it is a directive or label, and then see from there what to do
-        if(strcmp(t->tokens[i], ".text") == 0){
-            //process program code
-        }
-        if(strcmp(t->tokens[i], ".data") == 0){
-            //process static data
-        }
-        if(strcmp(t->tokens[i], ".bss") == 0){
-            //process uninit data
-        }
-
 
 
     }
+
+
+    return 0;
 
 }
