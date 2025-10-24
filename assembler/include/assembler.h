fabcf0d66920741fe2d22abfacdc229ba33ebd60
Dan Mihovch <dmihovch@gmail.com>
2025-10-23 23:25:29 -0400

    underscores

--- assembler/include/assembler.h
+++ assembler/include/assembler.h
@@ -3,6 +3,9 @@
 
 
 int asm_first_pass(tokens_t*, label_table_t*);
-int isOpcode();
-int isDirective();
-int isLabel();
+
+//actually idk
+//return -1 for these if false, return the bytecode if true
+int is_opcode();
+int is_directive();
+int is_label();
